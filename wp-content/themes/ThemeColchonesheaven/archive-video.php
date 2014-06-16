84be024b1872efc56ad3ee77b4dc48fe1905aec7
Alfonsin <alfonsin.marquez@altra.com.bo>
2014-06-16 16:27:43 -0400

    edit css

--- wp-content/themes/ThemeColchonesheaven/archive-video.php
+++ wp-content/themes/ThemeColchonesheaven/archive-video.php
@@ -1,5 +1,4 @@
-<?php 
-get_header(); ?>
+<?php get_header(); ?>
 <section id="site_content" class="wrapper">
     <div id="id_menu_active" class="video"></div>
     <div class="wrapper_content">
@@ -11,72 +10,41 @@ get_header(); ?>
             </div>
             <div class="wrapp_cvideo">
                 <?php
-                    $type = 'video';
-                    if(empty($_GET['id_post'])){
-                        $args=array(
-                            'post_type' => $type,
-                            'post_status' => 'publish',
-                            'orderby' => 'date',
-                            'order' => 'DESC',
-                            'posts_per_page'=>1
-                        );
-                    }else{
-                        $args=array(
-                            'post_type' => $type,
-                            'post_status' => 'publish',
-                            'orderby' => 'date',
-                            'order' => 'DESC',
-                            'posts_per_page'=>1,
-                            'p'=>$_GET['id_post']
-                        );
-                    }             
-                     $myposts = new WP_Query( $args );
-                     if ( $myposts->have_posts() ) : 
-                          while ( $myposts->have_posts() ):$myposts->the_post();   ?>
-                            <div class="video_list">
-                                <?php the_field('id_video'); 
-                                    $id_post_actual=get_the_ID();
-                                ?>
-                            </div>
-                <?php     endwhile; 
-                          wp_reset_query();
-                          else: ?>
-                          <p><?php _e('Sorry, no slider matched your criteria.'); ?></p>
-                <?php endif; ?>
-                <div class="block_list_video">
-                    <ul class="video_slider">
-                        <?php
-                            $type = 'video';
-                            $args=array(
-                                'post_type' => $type,
-                                'post_status' => 'publish',
-                                'orderby' => 'date',
-                                'order' => 'DESC',
-                                'posts_per_page'=>-1
-                            );
-                             $myposts = new WP_Query( $args );
-                             if ( $myposts->have_posts() ) : 
-                                  while ( $myposts->have_posts() ):$myposts->the_post();   
-                                        $featured = wp_get_attachment_image_src(get_post_thumbnail_id(get_the_ID()),'thumb-imagen-small',false); 
-                                        $select="";
-                                        if ($id_post_actual==get_the_ID()) {
-                                            $select="select_video";
-                                        }
-                                         ?>
-                                        <li>
-                                            <a href="/demo/video/?id_post=<?php echo get_the_ID(); ?>">
-                                                <figure class="dir_video <?php echo $select; ?>">
-                                                    <img src="<?php echo $featured[0]; ?>" width="37" height="31"/>
-                                                    <span class="log_vedeo"></span>
-                                               </figure>
-                                           </a>
-                                        </li>
+                $args = Array(
+                    'users' => 'UCc9BmSQXOK0f40r-t3AIt-Q',
+                    'countRows' => '6'
+                );
+                $youtube = new youtubeFeed($args);
+                $data = $youtube->getFeedByUserName();
+                ?>
+                <div class="video_list">   
+                    <?php                    
+                    $embed = $data[0]['embedUrl'];
+                    if (isset($_GET['embed'])) {
+                        $embed = base64_decode($_GET['embed']);
+                    }
+                    ?>
+                    <embed width="420" height="345" type="application/x-shockwave-flash" src="<?php echo $embed; ?>">                    
+                </div>
 
-                        <?php     endwhile; 
-                                  wp_reset_query();
-                              else: ?>
-                                  <p><?php _e('Sorry, no slider matched your criteria.'); ?></p>
-                       <?php endif; ?>
+                <div class="block_list_video">
+                    <ul class="video_slider">     
+                        <?php
+                        foreach ($data as $value) {
+                            ?>
+                            <li>
+                                <a href="?embed=<?php echo base64_encode($value['embedUrl']); ?>">
+                                <!--<a href="<?php echo $_SERVER["REQUEST_URI"]; ?>?embed=<?php echo $value['embedUrl']; ?>">-->
+                                    <figure class="dir_video <?php echo $select; ?>">
+                                        <img src="<?php echo $value['thumbnail']; ?>" title="<?php echo $value['title']; ?>" width="37" height="31"/>
+                                        <span class="log_vedeo"></span>
+                                    </figure>
+                                </a>
+                            </li>
+                            <?php
+                        }
+                        ?>
+<!--<p>sorry</p>-->                        
                     </ul>
                 </div>
             </div>
